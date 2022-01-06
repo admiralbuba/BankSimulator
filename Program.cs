ba4a2ef8298310fbf349dbbebbb3b8ba5fe6b710
user <bubonbs@gmail.com>
2022-01-06 16:05:13 +0100

    improved demo

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ using (ApplicationContext db = new())
     pc.Notify += Console.WriteLine;
 
     Bank bank = db.Banks.FirstOrDefault(b => b.Id == 1);
+    bank.ProcessingCenter = pc;
     Client client = db.Clients.FirstOrDefault(c => c.Id == 1);
     Client client1 = db.Clients.FirstOrDefault(c => c.Id == 2);
     Client client2 = db.Clients.FirstOrDefault(c => c.Id == 3);
@@ -17,29 +18,7 @@ using (ApplicationContext db = new())
     Account account2 = db.Accounts.FirstOrDefault(a => a.Id == 3);
     Card card1 = db.Cards.FirstOrDefault(c => c.Id == 1);
     Card card2 = db.Cards.FirstOrDefault(c => c.Id == 2);
-
-    //Market market = new() { Id = 1, Name = "Coffee", AccountId = 3, Account = account2 };
     Market market = db.Markets.FirstOrDefault(m => m.Id == 1);
-    //Bank bank = new("Prior", pc);
-    //Client client = new() { Name = "Kate", Bank = bank, BankId = bank.Id };
-    //Client client1 = new() { Name = "Mary", Bank = bank, BankId = bank.Id };
-    //Client client2 = new() { Name = "Market", Bank = bank, BankId = bank.Id };
-    //Account account = new() { Id = 1, ClientId = client.Id, Client = client, Card = new(), Sum = 50 };
-    //Account account1 = new() { Id = 2, ClientId = client1.Id, Client = client1, Card = new() };
-    //Account account2 = new() { Id = 3, ClientId = client2.Id, Client = client2, Card = new() };
-    //Card card1 = new() { Account = account, AccountId = account.Id };
-    //Card card2 = new() { Account = account1, AccountId = account1.Id };
-    //Market market = new() { Id = 1, Name = "Coffee", AccountId = 3, Account = account2 };
-
-    bank.ProcessingCenter = pc;
-    //db.Banks.Add(bank);
-    //db.Clients.Add(client);
-    //db.Clients.Add(client1);
-    //db.Accounts.Add(account);
-    //db.Accounts.Add(account1);
-    //db.Cards.Add(card1);
-    //db.Cards.Add(card2);
-    //db.SaveChanges();
 
     var cards = db.Cards
         .Include(c => c.Account)
